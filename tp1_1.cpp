18e25339c1017977bc7889b217debb9bed345c81
“marianogk” <marianokpo7@gmail.com>
2024-03-26 15:36:03 -0300

    punto e completo

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,7 +2,16 @@
 
 int main()
 {
+    int a = 5;
+    int *p;
+    p = &a;
+
     puts("hola mundo");
-    
+
+    printf("Contenido del puntero: %d \n", *p);
+    printf("Direccion de memoria almacenada en el puntero: %d \n", p);
+    printf("Direccion de memoria de la variable: %d \n", &a);
+    printf("Direccion de memoria del puntero: %d \n", &p);
+    printf("Tamano de memoria de la variable: %d \n", sizeof(int));
     return 0;
 }
